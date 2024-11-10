a6aba0706ffead22e14c9693446594628400219b
William Bonfim <gbo.william@gmail.com>
2024-11-10 21:02:20 +0000

    zmk-hammerbeam-display

---
+++ boards/dongle_display/shields/widgets/colors.h
@@ -0,0 +1,15 @@
+/*
+ *
+ * Copyright (c) 2024 The ZMK Contributors
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include <lvgl.h>
+#ifndef COLORS_H
+#define COLORS_H
+
+#define LVGL_FOREGROUND lv_color_hex(0xFFFFFF)  // Replace with desired color
+#define LVGL_BACKGROUND lv_color_hex(0x000000)  // Set to black or another color
+
+#endif // COLORS_H
